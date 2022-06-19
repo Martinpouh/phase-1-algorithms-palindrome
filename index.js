177ae3c58215a1db212df85101ddf795aabd9ff1
Martinpouh <martinchege2015@gmail.com>
2022-06-19 16:56:38 +0300

    commit tree 4d35444190116915b9c383467130dd552f769883

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    let array = word.split("")
+    let reversedArray = array.reverse()
+    let reverseWord = reversedArray.join("")
+    if (word == reverseWord) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
 
 /* 
   Add your pseudocode here
@@ -12,14 +21,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
